2731663b11e7909deb2f0293e40b04ec46f4b3ac
fengchang <fengchang08@gmail.com>
2017-06-09 17:52:26 +0800

    use redis to cache content

--- rss.php
+++ rss.php
@@ -5,6 +5,7 @@
  * Date: 2017/6/7
  * Time: 下午10:49
  */
+
 require 'vendor/autoload.php';
 
 use Monolog\Logger;
@@ -20,17 +21,32 @@ $log->pushHandler(new StreamHandler('php://stdout', Logger::DEBUG));
 $log->pushHandler(new StreamHandler('/opt/logs/bayes/readhub-rss/info.log', Logger::INFO));
 //$log->pushHandler(new LogglyHandler('2b26297e-cd7a-4902-812f-c481dcc2b5c2/tag/monolog', Logger::INFO));
 
-$readhub_data = json_decode(file_get_contents('https://api.readhub.me/topic?pageSize=20'), True);
+$redis = new Redis();
+$redis->pconnect('127.0.0.1', 6379);
 
-$feed = init_feed();
-foreach ($readhub_data['data'] as $readhub_item) {
-    $feed_item = $feed->createNewItem();
-    fill_feed_item($feed_item, $readhub_item);
-    $feed->addItem($feed_item);
+$content = $redis->get('topics');
+
+if ($content === FALSE) {
+
+    $readhub_data = json_decode(file_get_contents('https://api.readhub.me/topic?pageSize=20'), True);
+
+    $feed = init_feed();
+    foreach ($readhub_data['data'] as $readhub_item) {
+        $feed_item = $feed->createNewItem();
+        fill_feed_item($feed_item, $readhub_item);
+        $feed->addItem($feed_item);
+    }
+    $content = $feed->generateFeed();
+    $redis->set('topics',$content, 200);
+
+    $log->addInfo("API: Get ".count($readhub_data['data'])." items. Newest is ".$readhub_data['data'][0]['id']);
+} else {
+    $log->addInfo("Redis: Get content from Redis");
 }
-$feed->printFeed();
 
-$log->addInfo("API: Get ".count($readhub_data['data'])." items. Newest is ".$readhub_data['data'][0]['id']);
+
+header("Content-Type: application/atom+xml; charset=utf-8");
+echo $content;
 
 /**
  * @return ATOM
